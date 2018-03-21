a151ad25698b9e0ec075e91805c2ac5a4969e145
Hishengs <819537918@qq.com>
2018-03-21 17:05:52 +0800

    first commit

---
+++ src/components/button/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <button class="panda-button" :class="cClass" :disabled="disabled" @click="onclick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      type: {
+        type: String,
+        default: 'default',
+        validator: type => {
+          return ['default', 'primary', 'success', 'info', 'warning', 'error', 'text'].includes(type);
+        },
+      },
+      size: {
+        type: String,
+        default: 'normal',
+        validator: size => {
+          return ['mini', 'small', 'normal', 'large', 'warning'].includes(size);
+        },
+      },
+      round: Boolean,
+      block: Boolean,
+      loading: Boolean,
+    },
+    computed: {
+      cClass (){
+        return [
+          this.type,
+          this.disabled ? 'disabled' : '',
+          this.round ? 'round' : '',
+          this.block ? 'block' : '',
+          this.loading ? 'loading' : '',
+        ].filter(cls => cls !== '').join(' ');
+      },
+    },
+    methods: {
+      onclick (e){
+        this.$emit('click', e);
+      },
+    },
+  };
+</script>
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Button from './components/button';
+
+module.exports = exports = {
+  Button,
+};
